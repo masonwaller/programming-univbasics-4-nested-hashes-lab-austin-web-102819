03bfc4c9c2a7b3cb0c8c3261e5026980ddd6b54b
Mason Waller <masonwaller805@gmail.com>
2019-10-06 18:27:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,7 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      programmer_hash[:alan_key][:alans_new_info] = "GUI"
-     programmer_hash.delete[:alan_key][:known_for]
+     programmer_hash[:alan_key][:known_for]= "GUI"
 return programmer_hash     
      
 end
