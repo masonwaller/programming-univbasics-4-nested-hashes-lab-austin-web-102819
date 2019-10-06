ec4ee4776a15d6a6825c79ee6e19e35350060ab9
Mason Waller <masonwaller805@gmail.com>
2019-10-06 18:09:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -54,7 +54,8 @@ def dennis_ritchies_language
           :known_for => "Unix",
           :languages => ["C"]
         }
-     } return programmer_hash[:dennis_ritchie][:languages]
+     } 
+     return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
